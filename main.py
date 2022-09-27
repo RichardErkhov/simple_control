0537b84fd50d4176f1059d53b284dbc1a2e3871a
RichardErkhov <104026872+RichardErkhov@users.noreply.github.com>
2022-09-27 17:27:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ def click_button(button: str, length: float) -> None:
 #move the mouse relative to it's starting point
 def move_mouse(x: int, y: int, length: float) -> None:
     pyautogui.move(x,y,length)
+#type using click_button
+def typing(text: str, length: float) -> None:
+    dt = length/len(text)
+    for i in text:
+        click_button(i, dt)
+
+def mouse_press(left: bool) -> None:
+    #click left or right mouse button
+    ellipsis
+
+def mouse_hold(left: bool, length: float) -> None:
+    #hold left or right mouse button
+    ellipsis
+
+def mouse_drag(left: bool, length: float) -> None:
+    #hold left or right mouse button and move the mouse
+    ellpsis
